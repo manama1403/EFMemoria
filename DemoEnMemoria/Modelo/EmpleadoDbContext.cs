f48fe5eb200e79dc323070aeee6d0ad6156b40b9
Mauricio Cordero <mcorderog@compensarsalud.com>
2022-04-06 14:50:51 -0500

    Subir prueba EF memoria

---
+++ DemoEnMemoria/Modelo/EmpleadoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEnMemoria.Modelo
+{
+    public class EmpleadoDbContext : DbContext
+    {
+        public EmpleadoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Empleados>Empleados {get;set;}
+
+    }
+}
---
+++ DemoEnMemoria/Modelo/Empleados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoEnMemoria.Modelo
+{
+    public class Empleados
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string genero { get; set; }
+        public int edad { get; set; }
+        public decimal salario { get; set; }
+    }
+}
